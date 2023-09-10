c811a78e31c3737b7d133d59023623ae22d0e57a
kunal-thakur1 <91238202+kunal-thakur1@users.noreply.github.com>
2023-09-10 14:52:48 +0530

    Add files via upload

---
+++ Portfolio/Javascript/portfolio.js
@@ -0,0 +1,34 @@
+
+
+function scrollleft() {
+    var left = document.querySelector(".scroll-images");
+    left.scrollBy(400, 0)
+}
+
+function scrollright() {
+    var right = document.querySelector(".scroll-images");
+    right.scrollBy(-400, 0)
+}
+
+
+function scrollleftlang() {
+    var left = document.querySelector(".scroll-imageslang");
+    left.scrollBy(400, 0)
+}
+
+function scrollrightlang() {
+    var right = document.querySelector(".scroll-imageslang");
+    right.scrollBy(-400, 0)
+}
+
+function hidenav(){
+    var a=document.querySelector(".nav");
+    var b=document.getElementById('nav');
+    
+}
+
+
+
+
+
+
